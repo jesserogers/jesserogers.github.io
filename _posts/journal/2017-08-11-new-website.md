55fc91e27bb1f86b8e78d4559010032cff00ea5b
Jesse Rogers <jesse@jesserogers.co>
2017-08-15 21:30:08 -0700

    Misc updates and improvements
    Tidied up the gallery.js script and fixed some mobile formatting

--- _posts/journal/2017-08-11-new-website.md
+++ _posts/journal/2017-08-11-new-website.md
@@ -13,6 +13,8 @@ But I'm a _developer_, dammit, and I don't need to pay you guys $20 a month to h
 
 <img style="width: 100%;" src="{{"/assets/img/journal/2017-08-11-21-savage.jpg"}}" alt="Actor portrayal of me being frustrated with Squarespace by 21 Savage" title="Actor portrayal of me being frustrated with Squarespace by 21 Savage" />
 
+<span style=" display: block; margin: 1rem 0 1.618rem; font-size: 12px; color: #ccc;">Me realizing how much money I've paid Squarespace since 2013</span>
+
 And when I say "no time" I mean about three months, apparently? I started putting this site together in May (on a plane) and worked on it sporadically. Now it's August, and I will _not_ get billed by Squarespace again.
 
 ## So welcome to my new website!
@@ -25,11 +27,11 @@ I prioritized performance and efficiency throughout the codebase. HTTP requests 
 
 Plus, since this a statically generated site being served over a CDN (thanks <a href="http://cloudflare.com" target="_blank" title="Use Cloudflare, it's free and awesome">Cloudflare</a> ), it's inherently fast as hell. Performance-minded dev can only help to improve on that speed.
 
-This is also somewhat of a clean slate for me. At this point in my life, I feel like I can put up only the work that I _like_, rather than work I think might get me hired. I can write almost whatever and certainly whenever I want in this journal, so I think I'll do just that.
+This is also somewhat of a clean slate for me. At this point in my life, I feel like I can put up only the work that I _like_, rather than work I think might get me hired for something. I can write whatever and whenever I want in this journal/blog thing, so I think I'll do just that.
 
-I'll post new work here if I'm particularly stoked about about it, and I'll post random thoughts, sketches, life updates, and whatever else I feel like sharing here, too. I have ideas for some content I'd like to create, and being able to code a custom interface to host it would be nice.
+I'll post new work here if I'm particularly stoked about it, and I'll post random thoughts, sketches, life updates, and whatever else I feel like sharing here, too. I have ideas for some content I'd like to create, and being able to code a custom interface to host it would be nice.
 
-Idk, maybe I'll add a subscription component to this journal if I actually use it enough. Someday.
+Idk, maybe I'll add a subscription component to this journal if I actually use it enough. Baby steps.
 
 <a href="http://twitter.com/jesserogers" target="_blank">Follow me on Twitter in the meantime.</a>
 
--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -108,20 +108,28 @@ setInterval(function() {
 
   // Lightbox Gallery ----------
 
-$('.gallery-img').click(function() { //when user clicks on image
-  $('.gallery-img').addClass('is-visible');
-  $(this).children('.js-lightbox').fadeIn("slow", function() {
+var $galleryImg = $('.gallery-img');
 
-    // Lazy Load!
-    var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
-    // For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
-    if (typeof attr !== typeof undefined && attr !== false) { // Element has this attribute
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('src', function(){
-        return this.getAttribute('data-src');
-        $(this).removeAttr('data-src');
-      }).fadeIn(300);
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').removeAttr('data-src');
-    }
+$galleryImg.click(function() { //when user clicks on image
+  $galleryImg
+    .addClass('is-visible');
+  $(this).children('.js-lightbox')
+    .fadeIn("slow", function() {
+
+      // Lazy Load!
+      var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
+      // For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
+      if (typeof attr !== typeof undefined && attr !== false) { // Element has this attribute
+        $(this)
+          .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+          .attr('src', function(){
+            return this.getAttribute('data-src');
+            $(this).removeAttr('data-src');
+          }).fadeIn(300);
+        $(this)
+        .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+        .removeAttr('data-src');
+      }
 
     $('.js-lightbox-prev').on('click', prevImage); // Previous Image
     $('.js-lightbox-next').on('click', nextImage); // Next Image
@@ -132,37 +140,59 @@ $('.gallery-img').click(function() { //when user clicks on image
 
 var prevImage = function() {
   var $previous = $(this).parent('.js-lightbox').parent('.gallery-img').prev();
-  $(this).closest('.gallery-img').children('.js-lightbox').hide();
-  $previous.children('.js-lightbox').show(0, function() { // fade in next lightbox
 
-    var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
-    if (typeof attr !== typeof undefined && attr !== false) {
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('src', function(){
-        return this.getAttribute('data-src');
-        $(this).removeAttr('data-src');
-      }).fadeIn(300);
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').removeAttr('data-src');
-    }
+  $(this).closest('.gallery-img') // Find parent
+    .children('.js-lightbox') // Parent's lightbox
+    .hide(); // Hide it
+  $previous.children('.js-lightbox') // select lightbox
+    .show(0, function() { // show it
 
-  });
-  return false;
+      var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
+
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this)
+          .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+          .attr('src', function(){
+            return this.getAttribute('data-src');
+            $(this).removeAttr('data-src');
+          }).fadeIn(300);
+        $(this)
+        .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+        .removeAttr('data-src');
+      }
+
+    });
+    return false;
 }
 
 // Next lightbox ----------
 
 var nextImage = function() {
-  var $next = $(this).parent('.js-lightbox').parent('.gallery-img').next();
-  $(this).closest('.gallery-img').children('.js-lightbox').hide();
-  $next.children('.js-lightbox').show(0, function() {
 
-    var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
-    if (typeof attr !== typeof undefined && attr !== false) {
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('src', function(){
-        return this.getAttribute('data-src');
-        $(this).removeAttr('data-src');
-      }).fadeIn(300);
-      $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').removeAttr('data-src');
-    }
+  var $next = $(this).parent('.js-lightbox').parent('.gallery-img').next();
+
+  $(this).closest('.gallery-img') // Find parent
+    .children('.js-lightbox') // Select lightbox
+    .hide(); // Hide current lightbox
+  $next.children('.js-lightbox') // lightbox of next gallery image
+    .show(0, function() { // Show next lightbox
+
+      var attr = $(this)
+                  .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+                  .attr('data-src');
+
+      if (typeof attr !== typeof undefined && attr !== false) {
+        $(this)
+          .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+          .attr('src', function(){
+            return this.getAttribute('data-src');
+            $(this).removeAttr('data-src');
+          })
+          .fadeIn(300);
+        $(this)
+          .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+          .removeAttr('data-src');
+      }
 
   });
   return false;
@@ -170,26 +200,38 @@ var nextImage = function() {
 
 // Image meta button  ----------
 
-$('.js-lightbox-info-trigger').click(function() {
-  var trigger = $('.js-lightbox-info-trigger input');
-  trigger.toggleClass('hide-it-baby');
-  if (trigger.hasClass('hide-it-baby')) {
-    trigger.val('Hide Info');
+$('.js-lightbox-info-trigger').click(function() { // user clicks on info trigger
+
+  var $trigger = $('.js-lightbox-info-trigger input'),
+  $triggerWrap = $('.js-lightbox-info-trigger');
+
+  $trigger.toggleClass('hide-it-baby'); // toggle mod class
+
+  if ($trigger.hasClass('hide-it-baby')) { // when active
+    $trigger.val('Hide Info'); // swap input text to 'Hide Info'
   } else {
-    trigger.val('Show Info');
+    $trigger.val('Show Info'); // else say 'Show Info'
   }
-  $('.js-lightbox-info-trigger').next().slideToggle();
-  $('.js-lightbox-info-trigger').parent('.js-lightbox').toggleClass('is-showing-info');
+
+  $triggerWrap.next() // next element in markup is info section
+    .slideToggle(); // slide in
+
+  $triggerWrap.parent('.js-lightbox')
+    .toggleClass('is-showing-info'); // add mod class to parent lightbox
+
 });
 
 // Close Lightbox Button  ----------
 
 $('.js-lightbox-close').click(function(e) { // when user clicks X button
   e.stopPropagation();
-  $(this).parent('.js-lightbox').fadeOut("slow", function() {
-    $(this).css('display', 'none');
-  });
-  $('.gallery-img').removeClass('is-visible');
+  $(this)
+    .parent('.js-lightbox')
+    .fadeOut('slow', function() { // fade out light box
+      $(this)
+        .css('display', 'none');
+    });
+  $galleryImg.removeClass('is-visible'); // remove mod class for all gallery images
   return false;
 });
 
