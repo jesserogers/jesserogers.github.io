50aa669aecc762d178f38bd12d6c84d930b99c6b
Jesse Rogers <jesse@jesserogers.co>
2017-10-18 21:13:32 -0700

    Added swipe gesture support to lightbox
    Script improvements, logging around image loading, and uh, SWIPE
    SUPPORT, baby.

--- _includes/js/gallery.js
+++ _includes/js/gallery.js
@@ -4,71 +4,97 @@
   var $galleryImg = $('.gallery-img');
 
   // Lazy load
+  
   var loadImage = function() {
 
-    var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src');
-    // For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
+    var attr = $(this).find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe').attr('data-src'); // For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
+
     if (typeof attr !== typeof undefined && attr !== false) {
+
       $(this)
         .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe') // find images or videos in lightbox
         .attr('src', function(){ // target src attribute
           return this.getAttribute('data-src'); // get value of 'data-src' and put it into a 'src' attr
         }).fadeIn(300); // fade in image/video
+
+      console.log('Loaded ' + attr + ' into the DOM');
+
       $(this)
-      .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
-      .removeAttr('data-src');
+        .find('.js-lightbox-img-wrap img, .js-lightbox-img-wrap iframe')
+        .removeAttr('data-src');
+
     }
   }
 
+  // Previous Image
+
   function prevImg() {
-    if ( $('.gallery-img.is-current').is(':first-child') ) {
-      $('.gallery-img.is-current')
-        .removeClass('is-current')
+    if ( $('.gallery-img.is-current').is(':first-child') ) { // if current image is first in gallery
+      $('.gallery-img.is-current') // select current
+        .removeClass('is-current') // remove mod class
         .find('.js-lightbox')
-          .hide()
-      $('.gallery-img:last-child')
-        .addClass('is-current')
+          .hide() // hide current lightbox
+      $('.gallery-img:last-child') // select last image in gallery
+        .addClass('is-current') // add mod class
         .find('.js-lightbox')
-          .show();
+          .show(0, loadImage); // show lightbox and lazy load if necessary
     } else {
-      $('.gallery-img.is-current')
-        .removeClass('is-current')
-        .children('.js-lightbox')
-          .hide()
+      $('.gallery-img.is-current') // select current
+        .removeClass('is-current') // remove mod class
+        .find('.js-lightbox')
+          .hide() // hide current lightbox
         .parent()
-        .prev()
-          .addClass('is-current')
-          .children('.js-lightbox')
-            .show(0, loadImage);
+        .prev() // find previous image
+          .addClass('is-current') // add mod class
+          .find('.js-lightbox')
+            .show(0, loadImage); // show lightbox and lazy load if necessary
     }
     return false;
   }
 
+  // Next Image
+
   function nextImg() {
-    if ( $('.gallery-img.is-current').is(':last-child') ) {
-      $('.gallery-img.is-current')
-        .removeClass('is-current')
+    if ( $('.gallery-img.is-current').is(':last-child') ) { // if current image is last in gallery
+      $('.gallery-img.is-current') // select current
+        .removeClass('is-current') // remove mod class
         .children('.js-lightbox')
-          .hide()
-      $('.gallery-img:first-child')
-        .addClass('is-current')
+          .hide() // hide current lightbox
+      $('.gallery-img:first-child') // select first image in gallery
+        .addClass('is-current') // add mod class
         .find('.js-lightbox')
-          .show();
+          .show(0, loadImage); // show lightbox and lazy load if necessary
     } else {
-      $('.gallery-img.is-current')
-        .removeClass('is-current')
+      $('.gallery-img.is-current') // select current
+        .removeClass('is-current') // remove mod class
         .children('.js-lightbox')
-          .hide()
+          .hide() // hide current lightbox
         .parent()
-        .next()
-          .addClass('is-current')
+        .next() // select next image
+          .addClass('is-current') // add mod class
           .find('.js-lightbox')
-            .show(0, loadImage);
+            .show(0, loadImage); // show lightbox and lazy load if necessary
     }
+    return false;
+  }
+
+  // Close Lightbox
+
+  function closeLightbox(e) {
+    $('.js-lightbox').fadeOut('slow', function() { // fade out lightbox
+      $(this).css('display', 'none'); // set CSS back to original value so it's not blank
+    });
+    $('.gallery-img')
+      .removeClass('is-visible is-current'); // remove mod classes from all images
+    $('html,body').css('overflow',''); // reset overflow property
+    e.stopPropagation();
     return false;
   }
 
   $galleryImg.click(function() { // user clicks on image
+
+    $('html, body').css('overflow','hidden');
+
     $galleryImg.addClass('is-visible'); // add mod class to all images
     $(this).addClass('is-current') // add mod class to clicked image
       .children('.js-lightbox')
@@ -89,14 +115,16 @@
 
   });
 
-  // Previous Image ----------
+  // Previous Image
+
   $('.js-lightbox-prev').click(prevImg);
 
-  // Next Image ----------
+  // Next Image
+
   $('.js-lightbox-next').click(nextImg);
 
 
-  // Image meta button  ----------
+  // Image meta button
 
   $('.js-lightbox-info-trigger').click(function() { // user clicks on info trigger
 
@@ -119,18 +147,23 @@
 
   });
 
-  // Close Lightbox Button  ----------
+  // Close Lightbox Button
 
-  $('.js-lightbox-close').click(function(e) { // when user clicks X button
-    e.stopPropagation();
-    $(this)
-      .parent('.js-lightbox')
-      .fadeOut('slow', function() { // fade out light box
-        $(this)
-          .css('display', 'none');
-      });
-    $galleryImg.removeClass('is-visible is-current'); // remove mod class for all gallery images
-    return false;
+  $('.js-lightbox-close').click(closeLightbox);
+
+  // Swipe Gestures
+
+  $('.js-lightbox').swipe({ // user swipes on slide
+  	swipeRight:function() { // user swipes right <3
+  		prevImg(); // run previous image function
+  	},
+  	swipeLeft:function() { // user swipes left </3
+  		nextImg(); // run next image function
+  	},
+    swipeDown:function() { // user swipes down
+      closeLightbox(); // close lightbox function
+    },
+  	threshold:178 // min swipe length of 178px
   });
 
 })();
