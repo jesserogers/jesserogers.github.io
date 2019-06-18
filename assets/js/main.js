04d0c9f2ebada23ba2de3f54c419df1c6f75f6a8
Jesse Rogers <jesse@jesserogers.net>
2019-06-18 10:42:33 -0700

    Nitpicking at js before I rewrite all of it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 ---
----
-function isTouch() { // check to see if touch device before DOM ready
+  ---
+var isTouch = function () { // check to see if touch device before DOM ready
 
   try {
     document.createEvent("TouchEvent");
     return true;
   }
-  catch(e) {
+  catch (e) {
     return false;
   }
 
 }
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
   // party time
-  {% include js/lazy.js %}
-  {% include js/nav.js %}
-  {% include js/occupations.js %}
-  {% include js/directions.js %}
-  {% include js/visible.js %}
-  {% include js/onscroll.js %}
-  {% include js/gallery.js %}
-  {% include js/journal.js %}
+  {% include js / lazy.js %}
+  {% include js / nav.js %}
+  {% include js / occupations.js %}
+  {% include js / directions.js %}
+  {% include js / visible.js %}
+  {% include js / onscroll.js %}
+  {% include js / gallery.js %}
+  {% include js / journal.js %}
 });
